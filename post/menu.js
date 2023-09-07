c5c68f9512e5b8eff47d62e6c91ce0b7c9e7270a
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-09-07 22:09:09 +0300

    Create menu.js

---
+++ post/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    
+    var menuLink = ' <li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io/cloud/">Cloud</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
+  
